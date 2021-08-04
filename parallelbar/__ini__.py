ffb80f959b5eceeb9552114cb7aae610b580c14d
padu <48981398+dubovikmaster@users.noreply.github.com>
2021-08-04 17:29:35 +0300

    clear __init_.py

--- parallelbar/__ini__.py
+++
@@ -1,1 +0,0 @@
-from .parallelbar import progress_map
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='parallelbar',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     author='Dubovik Pavel',
     description='Parallel processing with progress bars',
